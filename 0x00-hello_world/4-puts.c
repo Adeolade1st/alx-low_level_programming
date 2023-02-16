2fbeee44feae08316d15dd4cb720cc86c5280800
Adeolade1st <raisingsrs@gmail.com>
2023-02-16 15:45:46 -0800

    Name git commit -m Name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- *main- entry point
- *Description:  puts function to display 
- *Return: Always 0 (success)
- */
+* main - Entry point
+*
+* Description: Program to display with puts
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+	puts("Programming is like building a multilingual puzzle"\n);   
+       	return (0);
 }
