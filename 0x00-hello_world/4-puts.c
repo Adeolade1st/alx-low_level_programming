e74b3e5e01bfcf53d5f7beb056f2b0de7a8a7446
Adeolade1st <raisingsrs@gmail.com>
2023-02-16 13:45:52 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+/*puts function to display */
+
 
 int main(void)
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	   /* printf function displays the content that is
+	    *     * passed between the double quotes.
+	    *         */
+	   printf("with proper grammar, but the outcome is a piece of art,");
+	      return 0;
