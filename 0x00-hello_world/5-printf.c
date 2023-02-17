441ab1b25c982fb3f082f3c936e274eb52f233f0
Adeolade1st <raisingsrs@gmail.com>
2023-02-17 02:13:37 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-int main(void)
 
-/**
- * Main: Code execution starts here returns 0.
- *
- * Description: To  printf function displays the content that is
- * passed between the double quotes.
- *
- */
-{	
-	  printf("with proper grammar, but the outcome is a piece of art,\n");
-	  return 0;
-
-}
