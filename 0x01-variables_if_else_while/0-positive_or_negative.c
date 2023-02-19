72f9684b1ab0202cf8ce1a3dd057a67033dd5c8b
Adeolade1st <raisingsrs@gmail.com>
2023-02-19 13:24:04 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-/* Description: A conditional program to print positive, zero and negative number */
-
-/* main- The program begins here */
-
-/* return: 0 (success) */
-
+/**
+ * main - Program to assign random numbers to variables
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-        int n;
+	intn;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        { 
-	printf("Input a number:");
-	scanf("%d", &n);
-	(n > 0);
-	printf("%d is positive \n", n);
-	(n = 0);
-	scanf("%d", &n);
-	printf("%d is zero \n", n);
-	(n < 0);
-	scanf("%d", &n):;
-	printf("%d is negative \n", n);
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n,n");
 	}
+	if (n < 0)
+	{
+		printf("%d is negative\n,n");
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n,n");
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,17 +12,17 @@ int main(void)
 	srand(time(0);
 	n = rand() - RAND_MAX / 2;
 
-	 printf("Please Enter digit:" ");
-	 scanf( " % d ", &n );
-	 if ( n > 5 )
+	 printf("Please Enter digit:");
+	 scanf(" % d ", &n);
+	 if (n > 5)
 	 {
-	 printf("and is greater than 5\n");
+	 printf("is greater than 5\n");
 	 }
-	 else if ( n == 0 )
-	 printf("and is 0\n");
+	 else if (n = = 0)
+	 printf("is 0\n");
 	 {
-	 else if ( n<6 && n !=0 )
-	 printf("and is less than 6 and not 0\n);
+	 else if (n < 6 && n != = 0)
+	 printf("is less than 6 and not 0\n);
 	 }
 
 	return (0);
