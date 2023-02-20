6c957254d587798c1b981ae0eca805fa537e2579
Adeolade1st <raisingsrs@gmail.com>
2023-02-20 06:33:15 -0800

    print a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description : program that print alphabet in lower case
+ *
+ * retun: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alphabet = 'a'
+	{
+		while (alphabet <= 'z')
+			putchar(alphabet);
+
+	alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}
+
