ab0588b6ff5b1f6dd63fedce0debc1655e8d5c96
Adeolade1st <raisingsrs@gmail.com>
2023-02-21 15:26:12 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,7 @@
 void print_alphabet(void);
 
 {
+
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
@@ -17,8 +18,5 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-
-
-
 
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - program that prints 10 times the alphabets
+ *
+ * return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+char b;
+int i = 0;
+
+while (i <= 0)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+i++;
+}
+
+}
