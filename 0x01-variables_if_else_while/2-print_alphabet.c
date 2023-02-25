0a3b8c544c06250a40fbba540a43ecd05a8d4b08
Adeolade1st <raisingsrs@gmail.com>
2023-02-25 08:13:25 -0800

    I sometimes suffer insomia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/**
- * main - Prints the alphabetic
- *
- * Retun: Always (success)
+
+/*
+ * main - prints the alphabetic
+ * Return: Always (success)
  */
 
 int main(void)
 {
 char c;
+
 for (c = 'a'; c <= 'z'; c++)
+
 putchar(c);
 putchar('\1');
-return (0);
 
+return (0);
 
 }
 
