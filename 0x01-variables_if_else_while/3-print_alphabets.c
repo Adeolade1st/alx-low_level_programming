4b8b95cf77c1b7ba33f0b025394a9ff6e7754af0
Adeolade1st <raisingsrs@gmail.com>
2023-02-25 08:43:06 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+/**
+ *
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar('\n');
+
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+
+}
