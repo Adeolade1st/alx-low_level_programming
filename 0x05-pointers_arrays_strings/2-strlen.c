977146aaf3be249c4f7a068bd03da27b69c14c2f
Adeolade1st <raisingsrs@gmail.com>
2023-02-28 13:33:06 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* strlen - program that returns the length of a string
+* @s: string
+* return: long
+*/
+
+int_strlen(char *s)
+
+{
+
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+
+return (longi);
+
+}
