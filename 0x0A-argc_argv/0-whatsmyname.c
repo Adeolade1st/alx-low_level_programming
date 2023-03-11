eefb2e6d9b7c8628028efa03460d7bae0ee71cb6
Adeolade1st <raisingsrs@gmail.com>
2023-03-11 09:26:18 -0800

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @argc: counts the program
+ * @argv: to store
+ * Programe to print name
+ * return: name
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+(void)argc;
+(void)argv;
+
+printf("./mynameis %c\n", argc);
+
+return (0);
+}
