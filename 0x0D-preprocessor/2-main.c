315a0a226d59b4bd9266a06c046785e032b14e46
Adeolade1st <raisingsrs@gmail.com>
2023-03-20 17:15:54 -0700

    A_function_like_macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _program_that_prints - to print program it was compiled from
+ * @argc: argument counter
+ * @argv: stores the argument
+ * return: the program name
+ */
+
+int main(int argc, char *argv[])
+
+{
+printf("argv[0]\n");
+
+return(0);
+
+}
+
