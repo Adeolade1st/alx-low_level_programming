b363c6bebb6b3941a2702116b3b0ec4cd46d08d3
Adeolade1st <raisingsrs@gmail.com>
2023-03-20 18:00:32 -0700

    02-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * function - program to print program  where it was compiled from
+ * return: 0 success
+ */
+
+int main(void)
+
+{
+printf("%s\n", __FILE__);
+
+return (0);
+
+}
+
