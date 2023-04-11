b6f3b1ac98632c7e265a35422f20715aebfe8ea7
Adeolade1st <raisingsrs@gmail.com>
2023-04-11 14:20:31 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 
 #include <stdlib.h>
@@ -11,4 +11,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 
-#endif   /** MAIN_H */
+#endif
